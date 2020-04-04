29aefc72d86bb464f246e69ae5fb2d2cc39fc74f
Tilo Werdin <tilo.werdin@gmail.com>
2020-04-04 20:02:58 +0200

    safety

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import RPi.GPIO as GPIO
 import time
 
 if __name__ == '__main__':
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(23, GPIO.OUT)
-    
-    for i in range(5):
-        GPIO.output(23, GPIO.HIGH)
-        time.sleep(0.5)
-        GPIO.output(23, GPIO.LOW)
-        time.sleep(0.5)
+    try:
+        print(f"setmode to BCM and setup GPIO 23 to OUT")
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(23, GPIO.OUT)
+        
+        for i in range(5):
+            print(f"i = {i}")
+            GPIO.output(23, GPIO.HIGH)
+            time.sleep(0.5)
+            GPIO.output(23, GPIO.LOW)
+            time.sleep(0.5)
+    finally:
+        print(f"cleanup GPIO")
+        GPIO.cleanup()
