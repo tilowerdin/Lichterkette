5891ff6844583c297e53d3b30efa0d8f38885da8
Tilo Werdin <tilo.werdin@gmail.com>
2020-04-05 14:19:19 +0200

    add client

---
+++ client.py
@@ -0,0 +1,36 @@
+import socket
+import sys
+
+_PORT = 2345
+
+
+def sendToServer(msg):
+    # create an INET, STREAMing socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    print("trying to connect")
+    # s.connect((socket.gethostname(), _PORT))
+    s.connect(("raspberrypi", _PORT))
+    print("connected")
+
+    print("trying to send message")
+    s.send(msg.encode())
+    print("send message")
+
+    print("trying to receive message")
+    msg = s.recv(2048).decode()
+    print(f"got answer: {msg}")
+
+    print("cleanup")
+    s.close()
+
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    print(args)
+    if (len(args) > 1) and ("close" in args):
+        sendToServer("close")
+    else:
+        sendToServer("Hallo Welt!")
+
